b88ac917418503481cc397efb1814edc34b0eee7
camiilaaa <105816675+camiilaaa@users.noreply.github.com>
2022-10-05 16:52:15 -0300

    Add files via upload

--- MatterJSBoilerPlate-main/sketch.js
+++ MatterJSBoilerPlate-main/sketch.js
@@ -1,48 +1,69 @@
-var ball;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
+let engine;
+let world;
 
-function preload()
-{
-	
-}
+var ground;
+var left;
+var right;
+var top_wall;
+var ball;
+var btn1;
+var btn2;
 
 function setup() {
-	createCanvas(900,900);
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
 
-	engine = Engine.create();
-	world = engine.world;
+  btn1 = createImg('right.png');
+  btn1.position(220,30);
+  btn1.size(50,50);
+  btn1.mouseClicked(hForce);
 
-	//Create the Bodies Here.
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		friction:0,
-		density:1.2
-	}
+  btn2 = createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+  
+  
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
 
+  var ball_options ={
+    restitution: 0.95
+  }
+
+  ball = Bodies.circle(200,100,20,ball_options);
+  World.add(world,ball);
  
-	Engine.run(engine);
-   
-}
-
-
-function draw() {
   rectMode(CENTER);
-  background(0);
-  Matter.Bodies.circle(2,3,1)
-  drawSprites();
-  groundObj.display();
- groundObj=new ground(width/2,670,width,20);
- leftSide  = new ground(1100,600,20,120);
+  ellipseMode(RADIUS);
 }
 
-function keyPressed(){
-	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:0});
-	}
+function draw() 
+{
+  background(51);
+
+  ellipse(ball.position.x,ball.position.y,20);
+
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
 }
 
+function hForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+}
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+}
